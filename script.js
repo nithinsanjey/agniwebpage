3e526a497ec4612b2d6b7c1180351227c8a84377
simplyNitin <sanjeynithin@gmail.com>
2015-08-09 18:03:48 +0530

    page responsive min

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
 $(document).ready(function(){
 	$(".description").hide();
     $(".vertical_div").mouseover(function(){
-		$(this).css("-webkit-filter","grayscale(0%)");
+    	console.log("first vertical hovering "+$.inArray( this,$(".vertical_div") ));
+    	$(this).css("-webkit-filter","grayscale(0%)");
 		$(this).css("filter","grayscale(0%)");
+    	switch($.inArray( this, $(".vertical_div") ))
+    	{
+    		case 0:
+				$(this).next().next().next().next().css("-webkit-filter","grayscale(0%)");
+				$(this).next().next().next().next().css("filter","grayscale(0%)");
+				$(this).next().next().css("-webkit-filter","grayscale(0%)");
+				$(this).next().next().css("filter","grayscale(0%)");
+				break;
+			case 1:
+				$(this).next().next().css("-webkit-filter","grayscale(0%)");
+				$(this).next().next().css("filter","grayscale(0%)");
+				break;	
+			case 2:
+			$(this).prev().prev().css("-webkit-filter","grayscale(0%)");
+				$(this).prev().prev().css("filter","grayscale(0%)");
+				$(this).next().next().css("-webkit-filter","grayscale(0%)");
+				$(this).next().next().css("filter","grayscale(0%)");
+				break;
+			case 3:
+				$(this).prev().prev().css("-webkit-filter","grayscale(0%)");
+				$(this).prev().prev().css("filter","grayscale(0%)");
+				break;
+			case 4:
+				$(this).prev().prev().prev().prev().css("-webkit-filter","grayscale(0%)");
+				$(this).prev().prev().prev().prev().css("filter","grayscale(0%)");
+				$(this).prev().prev().css("-webkit-filter","grayscale(0%)");
+				$(this).prev().prev().css("filter","grayscale(0%)");
+				break;
+    	}
+		
 		//$(".vertical_div").css("-webkit-filter","grayscale(0%)");
 		$(this).children().next().children().children().next().slideDown("fast");
 		//$(this).children().css("background-color","rgba(0,0,0,0.4)");
