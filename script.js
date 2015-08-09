b0475a5b5958781b1907d1ece94cafaccbe57690
simplyNitin <sanjeynithin@gmail.com>
2015-08-09 13:23:38 +0530

    changed the table layout

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $(".vertical_div").mouseover(function(){
 		$(this).css("-webkit-filter","grayscale(0%)");
 		$(this).css("filter","grayscale(0%)");
-		$(".vertical_div").css("-webkit-filter","grayscale(0%)");
+		//$(".vertical_div").css("-webkit-filter","grayscale(0%)");
 		$(this).children().next().children().children().next().slideDown("fast");
 		//$(this).children().css("background-color","rgba(0,0,0,0.4)");
 		
